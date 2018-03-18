10c73e9ad82d673d74e2925f609ba1e993f7fc2b
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-18 11:16:57 +0000

    Added some studios/films to Runner to test the database is updated as expected.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,7 @@
 import db.DBHelper;
 import models.Film;
 import models.Studio;
+import sun.security.pkcs11.Secmod;
 
 public class Runner {
 
@@ -12,8 +13,17 @@ public class Runner {
         Studio studio2 = new Studio("Walt Disney Studios", 7000000.00);
         DBHelper.saveOrUpdate(studio2);
 
-        Film film1 = new Film("GGG", "XXX", 10000, studio1);
+        Studio studio3 = new Studio("Universal Pictures", 8000000.00);
+        DBHelper.saveOrUpdate(studio3);
+
+        Film film1 = new Film("Titanic", "Drama", 100000, studio1);
         DBHelper.saveOrUpdate(film1);
+
+        Film film2 = new Film("Interstellar", "Sci-Fi", 90000, studio1);
+        DBHelper.saveOrUpdate(film2);
+
+        Film film3 = new Film("Jurassic Park", "Action", 110000, studio3);
+        DBHelper.saveOrUpdate(film3);
 
 
 
