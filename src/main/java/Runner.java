641b27b85151ea554d56762a6042348df85eee00
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-18 10:07:03 +0000

    Added properties: id, name & budget to Studio class and all getters/setters,then added Hibernate annotation so that the properties for an instantiation of the Studio class can successfully be stored in the database.
    Added skeleton Employee class and TDD test class.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,12 @@
+import db.DBHelper;
+import models.Studio;
+
 public class Runner {
 
     public static void main(String[] args) {
+
+        Studio studio = new Studio("Paramount Pictures", 5000000.00);
+        DBHelper.saveOrUpdate(studio);
 
     }
 }
--- src/main/java/models/Studio.java
+++ src/main/java/models/Studio.java
@@ -1,4 +1,49 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="studios")
 public class Studio {
+
+    private int id;
+    private String name;
+    private double budget;
+
+    public Studio() {
+    }
+
+    public Studio(String name, double budget) {
+        this.name = name;
+        this.budget = budget;
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name="budget")
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
 }
